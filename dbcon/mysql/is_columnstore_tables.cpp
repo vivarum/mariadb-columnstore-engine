013822c435d8fcc3aa54a231bca93ec906993cb2
Vasily Kozhukhovskiy <kojuhovskiy@gmail.com>
2026-04-02 13:36:50 +0200

    MCSC: add ROW_COUNT_ESTIMATE column to information_schema.columnstore_tables

--- dbcon/mysql/is_columnstore_tables.cpp
+++ dbcon/mysql/is_columnstore_tables.cpp
@@ -28,9 +28,12 @@
 #include "calpontsystemcatalog.h"
 #include "dataconvert.h"
 #include "is_columnstore.h"
+#include "dbrm.h"
 
 // Required declaration as it isn't in a MairaDB include
 bool schema_table_store_record(THD* thd, TABLE* table);
+
+const int BYTE_PER_BLOCK = 8192;
 
 ST_FIELD_INFO is_columnstore_tables_fields[] = {
     Show::Column("TABLE_SCHEMA", Show::Varchar(64), NOT_NULL),
@@ -39,7 +42,33 @@ ST_FIELD_INFO is_columnstore_tables_fields[] = {
     Show::Column("CREATION_DATE", Show::Datetime(0), NOT_NULL),  // TODO: Make a date if possible
     Show::Column("COLUMN_COUNT", Show::SLong(0), NOT_NULL),
     Show::Column("AUTOINCREMENT", Show::SLong(0), NULLABLE),
+    Show::Column("ROW_COUNT_ESTIMATE", Show::ULonglong(0), NULLABLE),
     Show::CEnd()};
+
+static uint64_t calculateRowCountEstimate(BRM::DBRM* emp, BRM::OID_t firstColumnOid)
+{
+  if (!emp || firstColumnOid == 0)
+    return 0;
+
+  std::vector<struct BRM::EMEntry> entries;
+  int rc = emp->getExtents(firstColumnOid, entries, false, false, true);
+
+  if (rc != 0 || entries.empty())
+    return 0;
+
+  uint64_t totalRows = 0;
+
+  for (const auto& entry : entries)
+  {
+    if (entry.colWid > 0)
+    {
+      uint64_t rowsPerBlock = BYTE_PER_BLOCK / entry.colWid;
+      totalRows += (entry.HWM + 1) * rowsPerBlock;
+    }
+  }
+
+  return totalRows;
+}
 
 static int is_columnstore_tables_fill(THD* thd, TABLE_LIST* tables, COND* cond)
 {
@@ -49,6 +78,14 @@ static int is_columnstore_tables_fill(THD* thd, TABLE_LIST* tables, COND* cond)
 
   execplan::CalpontSystemCatalog csc;
   csc.identity(execplan::CalpontSystemCatalog::FE);
+
+  BRM::DBRM::refreshShmWithLock();
+  std::unique_ptr<BRM::DBRM> emp(new BRM::DBRM());
+
+  if (!emp || !emp->isDBRMReady())
+  {
+    return 1;
+  }
 
   if (cond)
   {
@@ -88,6 +125,33 @@ static int is_columnstore_tables_fill(THD* thd, TABLE_LIST* tables, COND* cond)
       }
 
       table->field[5]->store(tb_info.tablewithautoincr);
+
+      if (tb_info.numOfCols > 0)
+      {
+        execplan::CalpontSystemCatalog::RIDList ridList = csc.columnRIDs(it->second);
+        if (!ridList.empty())
+        {
+          BRM::OID_t firstColumnOid = ridList[0].objnum;
+          uint64_t rowCount = calculateRowCountEstimate(emp.get(), firstColumnOid);
+          if (rowCount > 0)
+          {
+            table->field[6]->set_notnull();
+            table->field[6]->store(rowCount, true);
+          }
+          else
+          {
+            table->field[6]->set_null();
+          }
+        }
+        else
+        {
+          table->field[6]->set_null();
+        }
+      }
+      else
+      {
+        table->field[6]->set_null();
+      }
 
       if (schema_table_store_record(thd, table))
         return 1;
